f8dbd85054a25a3a09e960e2fe171cb3d556505c
Shun Okawa <shunokawa@gmail.com>
2020-10-29 08:52:44 +0900

    モデルファイルの修正 作業完了

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+// 下記を追記
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Image extends Model
 {
-    //
+	// 下記を追記
+	use SoftDeletes;
 }
