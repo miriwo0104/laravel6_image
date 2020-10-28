445ffedcbaf9896303fa9ea63495e59d56df46b4
Shun Okawa <shunokawa@gmail.com>
2020-10-29 08:49:11 +0900

    マイグレーションファイルの記載とマイグレート 作業完了

---
+++ database/migrations/2020_10_28_234753_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            // 下記を追記
+            $table->string('file_path')->default('default');
+            $table->string('file_name')->default('default');
+            $table->softDeletes();
+            // 上記までを追記
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
