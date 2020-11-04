68d03de32c7eccc1793631eca98f3d9dccd59ba3
Shun Okawa <shunokawa@gmail.com>
2020-11-04 13:45:37 +0900

    コントローラファイルの記載 作業完了

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Image;
 use Illuminate\Support\Facades\Storage;
+// 下記を追記
+use ZipArchive;
+use Illuminate\Support\Str;
+// 上記までを追記
 
 class ImageController extends Controller
 {
@@ -64,11 +68,30 @@ class ImageController extends Controller
         return Storage::response($image_info['file_path'], $image_info['file_name']);
     }
 
-    // 下記を追記
     public function download(Request $request)
     {
         $image_info = Image::find($request['id']);
         return Storage::download($image_info['file_path'], $image_info['file_name']);
     }
-    // 上記までを追記する
+
+    // 下記を追記
+    public function zipDownload(Request $request)
+    {
+        $image_info = Image::find($request['id']);
+        $zip_tmp_file_path = Storage::path('zip_tmp/');
+        $zip_tmp_file_name = Str::random(40) . '.zip';
+        $zip_tmp_file_full_path = $zip_tmp_file_path . $zip_tmp_file_name;
+
+        $zip = new ZipArchive();
+        $zip->open($zip_tmp_file_full_path, ZipArchive::CREATE);
+        $zip->addFromString($image_info['file_name'], Storage::get($image_info['file_path']));
+        $zip->close();
+
+        ob_end_clean();
+
+        $headers = ['Content-Type' => 'application/zip'];
+
+        return response()->download($zip_tmp_file_full_path, 'image.zip', $headers)->deleteFileAfterSend(true);
+    }
+    // 上記までを追記
 }
